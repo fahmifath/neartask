a5be50473f3f327a467613ee3c8c9c302e97da96
fahmifath <fahmifthur@gmail.com>
2025-12-30 19:47:07 +0700

    Remove Task interface and add SubTask interface for improved task management structure

---
+++ src/types/subtask.ts
@@ -0,0 +1,7 @@
+export interface SubTask {
+  id: string;
+  task_id: string;
+  title: string;
+  is_done: boolean;
+  weight: number;
+}
---
+++ src/types/task.ts
@@ -0,0 +1,12 @@
+import { type SubTask } from "./subtask";
+
+export interface Task {
+  id: string;
+  title: string;
+  description?: string;
+  task_date: string;
+  deadline: string;
+  use_progress: boolean;
+  progress: number;
+  subtasks?: SubTask[];
+}
